6fccdac4add18f80ae756cbeb520aad6705027c2
s13g3lb4um <s13g3lb4um@gmail.com>
2017-07-22 01:11:52 -0500

    Estilos y navegación básica

--- censo/urls.py
+++ censo/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, static
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='Home'),
+    url(r'^cargar$', views.upload, name='Upload'),
 ]
--- censo/views.py
+++ censo/views.py
@@ -5,3 +5,6 @@ from .forms import create_registro_censo
 
 def home(request):
     return render(request, 'censo/home.html')
+
+def upload(request):
+    return render(request, 'censo/cargar.html')
